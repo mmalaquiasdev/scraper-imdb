4a6ffba97593934ec0b65bd36fe3db6c296f8e01
Mateus Malaquias <mateusmalaquiasdev@outlook.com>
2018-05-05 15:10:20 -0300

    Created unit test fetchURLToCheerioInstance

--- test/libs/fetch-url-cheerio-instance.test.js
+++ test/libs/fetch-url-cheerio-instance.test.js
@@ -1,5 +1,17 @@
 import test from 'ava'
+import { fetchURLToCheerioInstance } from '../../src/libs'
 
-test('DEMOOOO', t => {
-  t.pass()
+test('Simple fetch url to cheerio instance', t => {
+  const fetch = (url) => {
+    t.is(url, 'https://www.imdb.com/', 'url cannot be differente of arguments')
+
+    return Promise.resolve({
+      text: () => `<p>OK!</p>`
+    })
+  }
+
+  return fetchURLToCheerioInstance(fetch, 'https://www.imdb.com/')
+    .then(($) => {
+      t.is($.html(), `<html><head></head><body><p>OK!</p></body></html>`)
+    })
 })
