6bf256868b36f5b1e64d0ef1067f4924fbc23fe2
Mateus Malaquias <mateusmalaquiasdev@outlook.com>
2018-05-08 23:49:16 -0300

    feat scrap movie by id

--- src/core/scraper-movie-url.js
+++ src/core/scraper-movie-url.js
@@ -8,7 +8,22 @@ const scraperMovieURL = (fetch, imdbID) => {
 }
 
 const parseElementsToMovie = $ => ({
-  title: $('.title_wrapper h1').text().trim()
+  title: $('.title_wrapper h1').contents().first().text().trim(),
+  rating: $('meta[itemprop="contentRating"]').attr('content'),
+  duration: $('time[itemprop=duration]').contents().first().text().trim(),
+  genres: parseElementsToGenres($),
+  datePublished: $('meta[itemprop="datePublished"]').attr('content'),
+  ratingValue: $('span[itemprop="ratingValue"]').text(),
+  poster: $('div.poster a img').attr('src'),
+  summary: $('div.summary_text').text().trim()
 })
+
+const parseElementsToGenres = $ => {
+  const genres = []
+  $('span[itemProp="genre"]').map((_, element) => {
+    genres.push($(element).text())
+  })
+  return genres
+}
 
 module.exports = scraperMovieURL
